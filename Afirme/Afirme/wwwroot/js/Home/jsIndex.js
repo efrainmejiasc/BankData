b0a3e69e14fbc4570d8e72b6c1629ea7d6dad196
_efrainmejiasc <85888811+EfrainAS@users.noreply.github.com>
2022-02-01 09:18:49 -0400

    update

---
+++ Afirme/Afirme/wwwroot/js/Home/jsIndex.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function () {
+    console.log("index ready!");
+    ImgRotate(0,true);
+});
+
+
+function goToFunction() {
+    alert('Funcionalidad no disponible');
+}
+
+function ImgRotate(i, flag) {
+    var img = ['images/primaria.jpg', 'images/secundaria.jpg', 'images/terciaria.jpg'];
+
+    if (flag) {
+        $('#ourImg').attr("src", img[i]);
+        SetImg(i);
+    }
+    else {
+   
+        $('#ourImg').attr("src", img[i]);
+        setTimeout(SetImg, 8000,i);
+    }
+}
+
+function SetImg(value) {
+    var n = 0;
+
+    if (value === 0)
+        n = 1;
+    if (value === 1)
+        n = 2;
+    if (value === 2)
+        n = 3;
+
+    ImgRotate(n, false);
+}
+
+function Espera() {
+    return false;
+}
